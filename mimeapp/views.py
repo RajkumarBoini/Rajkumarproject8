16e456ec09273461312eb7888485be7095daefaa
RajkumarBoini <121873201+RajkumarBoini@users.noreply.github.com>
2023-01-28 19:22:39 +0530

    Update views.py
    small change in views.py

--- mimeapp/views.py
+++ mimeapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views import View
 
 data="""<table><tr><th>eid</th><th>ename</th><th>esal</th></tr>
-<tr><td>1001</td><td><td>scott</td><td>2000</td></tr>
+<tr><td>1001</td><td>scott</td><td>2000</td></tr>
 <tr><td>1002</td><td>blade</td><td>1500</td></tr>
 <tr><td>1003</td><td>miller</td><td>2500</td></tr></table>"""
 class htmlview(View):
